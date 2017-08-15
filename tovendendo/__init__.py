a4fab6f51298b5001f37f0a5d2f269ea148f04f8
Ana Paula Gomes <apgomes88@gmail.com>
2017-08-15 10:51:55 -0300

    Add picture to item

--- tovendendo/__init__.py
+++ tovendendo/__init__.py
@@ -2,15 +2,21 @@ import os
 from tovendendo.portal.views import portal
 from tovendendo.db import db
 from tovendendo.users.models import User, UserAdminView
-from tovendendo.items.models import Item, Category, ItemView
+from tovendendo.items.models import Item, Category, ItemView, images
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-
+from flask_uploads import configure_uploads
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('APP_SETTINGS', 'tovendendo.config.DevelopmentConfig'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+app.config['UPLOADED_IMAGES_DEST'] = 'data/images'
+app.config['UPLOADED_IMAGES_URL'] = '/data/images/'
+
+configure_uploads(app, (images))
 
 db.init_app(app)
 
--- tovendendo/items/models.py
+++ tovendendo/items/models.py
@@ -1,13 +1,19 @@
 from tovendendo.db import db
 from sqlalchemy_utils import ChoiceType
 from flask_admin.contrib.sqla import ModelView
-
+from flask_admin.form import ImageUploadField
+from flask_admin.model.fields import InlineFieldList
+import os
+from flask import Markup
+from flask_uploads import UploadSet, IMAGES
 
 items = db.Table(
     'categories_items',
     db.Column('category_id', db.Integer, db.ForeignKey('categories.id')),
     db.Column('item_id', db.Integer, db.ForeignKey('items.id'))
 )
+
+images = UploadSet('images', IMAGES)
 
 
 class Item(db.Model):
@@ -30,9 +36,10 @@ class Item(db.Model):
     quantity = db.Column(db.Integer, default=1, nullable=False)
     categories = db.relationship(
         'Category', secondary=items, backref=db.backref('items', lazy='dynamic'))
+    pictures = db.relationship('Picture', backref='items')
 
     def __repr__(self):
-        return 'Item %r' % (self.name)
+        return self.name
 
 
 class Category(db.Model):
@@ -42,10 +49,61 @@ class Category(db.Model):
     name = db.Column(db.String(100), index=True, unique=True, nullable=False)
 
     def __repr__(self):
-        return '%r' % (self.name)
+        return self.name
+
+
+class Picture(db.Model):
+    __tablename__ = 'pictures'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True)
+    filename = db.Column(db.String(128), unique=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    def __repr__(self):
+        return '%r' % (self.filename)
+
+    @property
+    def url(self):
+        return images.url(self.filename)
+
+    @property
+    def filepath(self):
+        if self.filename is None:
+            return
+        return images.path(self.filename)
+
+
+def _list_thumbnail(view, context, model, name):
+    if not model.pictures:
+        return ''
+
+    return Markup(
+        '<img src="{model.pictures}" style="width: 150px;">'.format(model=model)
+    )
 
 
 class ItemView(ModelView):
     form_choices = {'age': Item.TYPES}
     column_hide_backrefs = False
-    column_list = ('name', 'price', 'available_on', 'quantity', 'categories')
+    column_list = ('name', 'price', 'available_on', 'quantity', 'categories', 'pictures')
+    form_extra_fields = {
+        'pictures': InlineFieldList(
+            ImageUploadField('Picture', base_path='data/images', url_relative_path='images/',))}
+    column_formatters = {
+        'pictures': _list_thumbnail
+    }
+
+    def create_model(self, form):
+        base_path = 'data/' # os.path.join(os.getcwd(), 'data/')
+
+        # import pdb; pdb.set_trace()
+
+        pictures = []
+        for picture in form.pictures.data:
+            filename = base_path + picture.filename
+            picture.save(filename)
+            pictures.append(Picture(name=picture.filename, filename=filename))
+
+        form.pictures = pictures
+        super(ItemView, self).create_model(form)
